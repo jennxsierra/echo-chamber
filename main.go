05969fa6058c69f15ab9febbd92edd6017e96a0c
Jennessa Sierra <2021153908@ub.edu.bz>
2025-04-16 17:31:48 -0600

    Implemented concurrency to handle multiple client connections

--- main.go
+++ main.go
@@ -23,20 +23,20 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
-	// define the target host and port we want to connect to
 	listener, err := net.Listen("tcp", ":4000")
 	if err != nil {
 		panic(err)
 	}
 	defer listener.Close()
 	fmt.Println("Server listening on :4000")
-	// Our program runs an infinite loop
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting:", err)
 			continue
 		}
-		handleConnection(conn)
+		// Handle each connection in a separate goroutine
+		go handleConnection(conn)
 	}
 }
